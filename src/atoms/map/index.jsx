7dcf29becb4d20ce11c8b7e9084f12a15ca05f4e
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2023-04-23 19:47:10 +0200

    Update dependencies and fix deprecated call

--- src/atoms/map/index.jsx
+++ src/atoms/map/index.jsx
@@ -5,7 +5,6 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import StyledMapContainer from "./styled.js";
 import { Marker, Popup } from "react-leaflet";
 import MarkerIcon from "../mapmarker/index.jsx";
-import Icon from "../icon/index.jsx";
 
 const Map = ({ data }) => {
 	let position = [39.513882, 0.619564];
@@ -24,11 +23,11 @@ const Map = ({ data }) => {
 			zoomControl
 			center={position}
 			zoom={[15]}
-			scrollWheelZoom={true}
+			scrollWheelZoom
 		>
 			<TileLayer
 				url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGF1bHBhdWwxMjM0NSIsImEiOiJja3poMDE0aDMzOGgwMnJuOTgwcjZhMWNoIn0.1pYURF9UykgW3o_4CQZEqg"
-				zoomControl={true}
+				zoomControl
 			/>
 			<Marker position={position} icon={MarkerIcon}>
 				<Popup>The Ship is here!</Popup>
